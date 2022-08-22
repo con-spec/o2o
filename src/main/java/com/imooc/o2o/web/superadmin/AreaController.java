05f7f6e1043c2a14a8553035c997c983dc3de858
xwzuo <zxwbds@163.com>
2022-08-22 17:15:43 +0800

    添加logback

--- src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
@@ -3,6 +3,8 @@ package com.imooc.o2o.web.superadmin;
 import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.service.AreaService;
 import org.apache.commons.collections.ArrayStack;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,15 @@ import java.util.*;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = (Logger) LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
 
     @RequestMapping(value = "/listarea", method = RequestMethod.GET)
     @ResponseBody
-    private Map<String, Object> listArea() {
+    public Map<String, Object> listArea() {
+        logger.info("===start===");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modeMap = new HashMap<String, Object>();
         List<Area> list = new ArrayList<Area>();
         try {
@@ -31,6 +36,10 @@ public class AreaController {
             modeMap.put("success",false);
             modeMap.put("errMsg",e.toString());
         }
+        logger.error("test error");
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime-startTime);
+        logger.info("===end===");
         return modeMap;
     }
 }
